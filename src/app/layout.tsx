4592e1125c22fb2474d8bf1a4b4d6c65513d5d82
Sprocket Riggs <sprocket710@gmail.com>
2023-11-05 15:41:04 -0500

    Set up initial color scheme + typeface

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,29 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Roboto_Mono, Barlow } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+
+const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto-mono',
+})
+
+const barlow = Barlow({
+  weight: ['400', '500'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-barlow',
+})
+
+/*
+const onest = Onest({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-onest'
+})
+*/
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +36,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${inter.variable} ${roboto_mono.variable} ${barlow.variable}`}>
+      <body>{children}</body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,20 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    /* https://colorhunt.co/palette/27374d526d829db2bfdde6ed */
+    colors: {
+      'background-dark': '#1c1a11',
+      'background-light': '#27374D',
+      'primary-dark': '#526D82',
+      'primary-light': '#DDE6ED',
+      'accent': '#9DB2BF',
+    },
     extend: {
+      fontFamily: {
+        sans: ['var(--font-barlow)'],
+        mono: ['var(--font-roboto-mono)'],
+        barlow: ['var(--font-barlow)'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
